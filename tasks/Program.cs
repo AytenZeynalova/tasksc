96c6bfa6e7c24afce581178c2a6c005aedd5f3be
AytenZeynalova <14rvxpg@code.edu.az>
2022-03-16 11:56:01 +0400

    created

---
+++ tasks/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task-1
+            //1. Daxil edilmiş string dəyərinin içində rəqəm olub olmadığını tapan proqram
+
+            //string variable;
+            //variable = Console.ReadLine();
+            //for (int i = 0; i < variable.Length; i++)
+            //{
+            //    if (Convert.ToInt32(variable[0]) > 10 && Convert.ToInt32(variable[i]) < 1000)
+
+            //    {
+            //            {
+            //                Console.WriteLine("daxil edilen deyerin ichinde reqem vardir");
+            //            }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("daxil edilen deyerin ichinde reqem yoxdur");
+            //    }
+
+            //}
+
+            #endregion
+
+
+
+            #region Task-2
+            //2. Daxil edilmiş string  dəyərində  ardıcıl a ve l char-larının olub olmadığını tapan proqram.
+            //Misalçün alma, salam və s. kimi sözlərdə "vardir" yazdırmalıdır geriyə.
+            //string words;
+            //words = Console.ReadLine();
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    if (words[i] == 'a')
+            //    {
+            //       if( words[i + 1] == 'l')
+            //        {
+            //            {
+            //                Console.WriteLine("vardir");
+
+            //            }
+
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("yoxdur");
+            //        }
+
+
+            //    }
+
+
+            //}
+
+            #endregion
+
+
+            #region Task-3
+            //3. Verilmiş ədədin rəqəmlərinin ədədi ortasını tapan proqram
+            //int num;
+            //num = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //int sum = 0;
+            //double avg = 0;
+            //if (num > 0)
+            //{
+            //    count++;
+            //    sum += num % 10;
+            //    num /= 10;
+            //    avg = sum / count;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded menfidir");
+            //}
+            //Console.WriteLine(avg);
+
+            #endregion
+
+
+            #region Task-4
+            //4. Daxil edilmiş yazı siyahısında (string array) neçə sözdə rəqəm olduğunu tapan proqram
+            //string[] words = new string[5];
+            //int counter = 0;
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    words[i] = Console.ReadLine();
+
+            //}
+            //for (int i = 0; i < words.Length; i++)
+            //{
+
+            //}
+
+
+
+            #endregion
+
+        }
+    }
+}
